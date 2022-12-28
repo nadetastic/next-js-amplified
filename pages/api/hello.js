3013081f18dcf47f95cd02a8d51d8dffff675e88
Dan Kiuna <dkkiuna11@gmail.com>
2022-12-28 12:17:46 -0600

    added auth to api route

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,30 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// import { buffer } from "micro";
+// import Cors from "micro-cors";
+// import { NextApiRequest, NextApiResponse } from "next";
+import { withSSRContext } from "aws-amplify";
+import { Amplify } from "aws-amplify";
+import aws_exports from '../../src/aws-exports';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+Amplify.configure({
+  ...aws_exports,
+  // oauth: {
+  //     ...oauth,
+  // },
+  ssr: true,
+});
+
+export default async function handler(req, res) {
+  console.log("REQ", req.cookies);
+    const { Auth } = withSSRContext({ req });
+    try {
+        const user = await Auth.currentAuthenticatedUser();
+        console.log("GOT", user);
+        res.status(200).json({ response: JSON.stringify(user) })
+    } catch (e) {
+        console.log("ERERER", e);
+        res.status(500).json( { response: JSON.stringify(e) })
+    }
+
+  // res.status(200).json({ name: 'John Doe' })
 }
